bce7c238a4d0d3b35630e92f819767f4adbd86fe
renatocfrancisco <renatocfrancisco1999@gmail.com>
2024-06-17 17:23:41 -0300

    add requests for constant values

---
+++ routes/constant/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const limiter = require('../../middlewares/limiter')
+const { validGenres, validRatings, validSessionTypes } = require('./strings')
+
+const router = express.Router()
+
+router.use(limiter)
+
+router.get('/genres', (_req, res) => {
+    res.send(validGenres)
+})
+
+router.get('/ratings', (_req, res) => {
+    res.send(validRatings)
+})
+
+router.get('/session-types', (_req, res) => {
+    res.send(validSessionTypes)
+})
+
+module.exports = router
---
+++ routes/constant/strings.js
@@ -0,0 +1,21 @@
+const validGenres = [
+    'Ação',
+    'Aventura',
+    'Animação',
+    'Comédia',
+    'Drama',
+    'Fantasia',
+    'Terror',
+    'Romance',
+    'Suspense',
+    'Faroeste',
+    'Musical',
+    'Documentário',
+    'Ficção Cientifica',
+    'Histórico'
+]
+const validRatings = ['L', '10', '12', '14', '16', '18']
+
+const validSessionTypes = ['Dublado', 'Legendado', 'Nacional']
+
+module.exports = { validGenres, validRatings, validSessionTypes }
